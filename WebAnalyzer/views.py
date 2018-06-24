e44ae05d9bdd7b44ddb6dd6f18c179c438787e7f
DESKTOP-TDE98GM\Shin <kepler92kr@gmail.com>
2018-06-24 16:35:48 +0900

    Edit Viewset nubmer for limit the number of queries

--- WebAnalyzer/views.py
+++ WebAnalyzer/views.py
@@ -4,6 +4,7 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 
 # Create your views here.
+from AnalysisModule.config import VIEWSET_NUMBER
 from WebAnalyzer.models import *
 from WebAnalyzer.serializers import *
 from rest_framework import viewsets, generics
@@ -12,3 +13,6 @@ from rest_framework import viewsets, generics
 class ImageViewSet(viewsets.ModelViewSet):
     queryset = ImageModel.objects.all()
     serializer_class = ImageSerializer
+
+    def get_queryset(self):
+        return self.queryset.order_by('-token')[:VIEWSET_NUMBER]
