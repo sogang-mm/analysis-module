5d25156bd4d5b5fb7e98e60347e03c6ff1dbcfd9
JinhaSong <thejn07@gmail.com>
2019-11-05 02:19:01 +0900

    admin page 추가

--- WebAnalyzer/admin.py
+++ WebAnalyzer/admin.py
@@ -2,5 +2,10 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from .models import ImageModel, ResultModel, ResultPositionModel, ResultLabelModel
 
 # Register your models here.
+admin.site.register(ImageModel)
+admin.site.register(ResultModel)
+admin.site.register(ResultPositionModel)
+admin.site.register(ResultLabelModel)
