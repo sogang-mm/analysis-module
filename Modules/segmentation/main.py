2221f9feef1fd010f6fbfd663b59f2864b39d35c
JinhaSong <thejn07@gmail.com>
2019-12-01 23:53:28 +0900

    celery 실행 버그 수정

--- Modules/segmentation/main.py
+++ Modules/segmentation/main.py
@@ -48,10 +48,6 @@ class Segmentation:
 
         self.result = {}
 
-        for Opt, Arg in getopt.getopt(sys.argv[1:], '', [strParameter[2:] + '=' for strParameter in sys.argv[1::2]])[0]:
-            if Opt == '--model' and Arg != '': self.arg_Model = Arg
-            if Opt == '--thres' and Arg != '': self.arg_Thres = float(Arg)
-
         # end
         self.evaluation = True  # our data : False !
 
