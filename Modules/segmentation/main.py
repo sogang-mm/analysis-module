57e6b1495f13dccd12dd230cf9fe2409fc63b61f
JinhaSong <thejn07@gmail.com>
2019-12-01 23:52:10 +0900

    celery 실행 버그 수정

--- Modules/segmentation/main.py
+++ Modules/segmentation/main.py
@@ -4,6 +4,16 @@ import shutil
 import torch
 from torch.autograd import Variable
 from torch.utils.data import Dataset, DataLoader
+import torch._utils
+try:
+    torch._utils._rebuild_tensor_v2
+except AttributeError:
+    def _rebuild_tensor_v2(storage, storage_offset, size, stride, requires_grad, backward_hooks):
+        tensor = torch._utils._rebuild_tensor(storage, storage_offset, size, stride)
+        tensor.requires_grad = requires_grad
+        tensor._backward_hooks = backward_hooks
+        return tensor
+    torch._utils._rebuild_tensor_v2 = _rebuild_tensor_v2
 import os
 import numpy as np
 import pandas as pd
