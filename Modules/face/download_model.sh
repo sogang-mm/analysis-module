0bc7e38c4b8592e72a772bceb339017b44bace1e
Jinha Song <jhsong0729@sogang.ac.kr>
2022-03-29 11:26:42 +0900

    Add a script to download model file

---
+++ Modules/face/download_model.sh
@@ -0,0 +1,2 @@
+wget ftp://mldisk.sogang.ac.kr/vtt/face/missoh/190903.pb -O /workspace/Modules/face/190903.pb
+wget ftp://mldisk.sogang.ac.kr/vtt/face/missoh/190903.pkl -O /workspace/Modules/face/190903.pkl
