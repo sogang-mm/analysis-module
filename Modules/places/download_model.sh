d75efe2342d84717de1c61d207f33786b367f5eb
Jinha Song <jhsong0729@sogang.ac.kr>
2022-03-29 11:30:52 +0900

    Update download_model.sh

--- Modules/places/download_model.sh
+++ Modules/places/download_model.sh
@@ -1,2 +1,2 @@
 wget ftp://mldisk.sogang.ac.kr/vtt/place/place47/categories.txt -O /workspace/Modules/places/categories.txt
-wget ftp://mldisk.sogang.ac.kr/vtt/place/place47/place47.pth.tar -O /workspace/Modules/places/place47.pth.tar
+wget ftp://mldisk.sogang.ac.kr/vtt/place/place47/place47-210903.pth.tar -O /workspace/Modules/places/place47.pth.tar
