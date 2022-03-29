155321d6c061bd0d48f20f62945378c28f32df74
Jinha Song <jhsong0729@sogang.ac.kr>
2022-03-29 11:30:04 +0900

    Add a script to download model file

---
+++ Modules/places/download_model.sh
@@ -0,0 +1,2 @@
+wget ftp://mldisk.sogang.ac.kr/vtt/place/place47/categories.txt -O /workspace/Modules/places/categories.txt
+wget ftp://mldisk.sogang.ac.kr/vtt/place/place47/place47.pth.tar -O /workspace/Modules/places/place47.pth.tar
