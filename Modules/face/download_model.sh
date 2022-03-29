970c1b3a06e84436bfe828581079643dfd6ffe24
Jinha Song <jhsong0729@sogang.ac.kr>
2022-03-29 11:27:19 +0900

    Update download_model.sh

--- Modules/face/download_model.sh
+++ Modules/face/download_model.sh
@@ -1,2 +1,2 @@
-wget ftp://mldisk.sogang.ac.kr/vtt/face/missoh/190903.pb -O /workspace/Modules/face/190903.pb
-wget ftp://mldisk.sogang.ac.kr/vtt/face/missoh/190903.pkl -O /workspace/Modules/face/190903.pkl
+wget ftp://mldisk.sogang.ac.kr/vtt/face/missoh/210903.pb -O /workspace/Modules/face/210903.pb
+wget ftp://mldisk.sogang.ac.kr/vtt/face/missoh/210903.pkl -O /workspace/Modules/face/210903.pkl
